7bd455aa7425d07b0fe89cb6ae12b361a9015d70
tomike19 <kolajoelizabeth@gmail.com>
2022-03-18 16:04:48 -0700

    add consultant page

--- src/pages/AddNewConsultant/AddNewConsultant.js
+++ src/pages/AddNewConsultant/AddNewConsultant.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import DefaultLayout from '../../DefaultLayout/DefaultLayout'
 import DashBoardNavbar from '../../components/DashboardNavbar/DashboardNavbar'
-
+import '../../styles/AddNewConsultant.css'
 const AddNewConsultant = () => {
   return (
     <div>
@@ -10,36 +10,103 @@ const AddNewConsultant = () => {
         <div className="dashboard-right-content">
           <DashBoardNavbar />
           <div className="dashboard-details">
-            <div className="dashboard">
-              <h4>Register Marketing Consultants</h4>
-              <form>
-                <div class="form-group row">
-                  <label for="inputEmail3" class="col-sm-2 col-form-label">
-                    Email
-                  </label>
-                  <div class="col-sm-10">
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="inputEmail3"
-                      placeholder="Email"
-                    />
+            <div className="dashboard-form-details">
+              <h4 className="form-details-title">
+                Register Marketing Consultant
+              </h4>
+              <div className="add-consultant-form">
+                <form>
+                  <div class="form-group row">
+                    <label
+                      for="inputEmail3"
+                      class="col-sm-2 col-form-label label-title"
+                    >
+                      Firstname
+                    </label>
+                    <div class="col-sm-10">
+                      <input
+                        type="text"
+                        class="form-control "
+                        id="inputEmail3"
+                        placeholder="firstname"
+                      />
+                    </div>
                   </div>
-                </div>
-                <div class="form-group row">
-                  <label for="inputPassword3" class="col-sm-2 col-form-label">
-                    Password
-                  </label>
-                  <div class="col-sm-10">
-                    <input
-                      type="password"
-                      class="form-control"
-                      id="inputPassword3"
-                      placeholder="Password"
-                    />
+                  <div class="form-group row">
+                    <label
+                      for="inputPassword3"
+                      class="col-sm-2 col-form-label label-title"
+                    >
+                      Lastname
+                    </label>
+                    <div class="col-sm-10">
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="inputPassword3"
+                        placeholder=" Lastname"
+                      />
+                    </div>
                   </div>
-                </div>
-              </form>
+                  <div class="form-group row">
+                    <label
+                      for="inputPassword3"
+                      class="col-sm-2 col-form-label label-title"
+                    >
+                      Username
+                    </label>
+                    <div class="col-sm-10">
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="inputPassword3"
+                        placeholder="Username"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label
+                      for="inputPassword3"
+                      class="col-sm-2 col-form-label label-title"
+                    >
+                      Phone No
+                    </label>
+                    <div class="col-sm-10">
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="inputPassword3"
+                        placeholder="Phone No"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label
+                      for="inputPassword3"
+                      class="col-sm-2 col-form-label label-title"
+                    >
+                      Email
+                    </label>
+                    <div class="col-sm-10">
+                      <input
+                        type="email"
+                        class="form-control"
+                        id="inputPassword3"
+                        placeholder="email"
+                      />
+                    </div>
+                  </div>
+                  <select
+                    class="form-select"
+                    aria-label="Default select example"
+                  >
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                </form>
+              </div>
             </div>
           </div>
         </div>
